df101da32b7e5d0b188450a3f5cfb9bde9dff15b
Truong <truongtdph40209@fpt.edu.vn>
2024-09-24 01:00:29 +0700

    Add LoginTemplate component

---
+++ components/templates/LoginTemplate.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { StyleSheet, useColorScheme } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { View } from 'tamagui'
+
+import ContentTitle from '~/components/atoms/ContentTitle'
+import Footer from '~/components/molecules/common/Footer'
+import InputForm from '~/components/molecules/InputForm'
+import getColors from '~/constants/Colors'
+import useTranslation from '~/hooks/useTranslation'
+
+const LoginTemplate: React.FC = (): JSX.Element => {
+  const { t } = useTranslation()
+  const colors = getColors(useColorScheme())
+
+  return (
+    <SafeAreaView style={[styles.container, { backgroundColor: colors.white }]}>
+
+      <View marginTop={30}>
+        <ContentTitle
+          title={t('login.welcomeBack')}
+          subtitle={t('login.loginPrompt')}
+        />
+      </View>
+
+      <View justifyContent="center" marginTop={20}>
+        <InputForm
+          visibleInputWithIcons={false}
+          visibleForgotPassword={true}
+
+        />
+      </View>
+
+      <View flex={1} justifyContent="flex-end" >
+        <Footer
+          title={t('login.signupPrompt')}
+          subtitle={t('login.joinNow')}
+        />
+      </View>
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 20,
+    height: '100%',
+    padding: 20
+  }
+})
+
+export default LoginTemplate
