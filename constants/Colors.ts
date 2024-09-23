51534d9e2a7e1b9892fc8b0dd1223814f2094dcc
Truong <truongtdph40209@fpt.edu.vn>
2024-09-24 01:22:40 +0700

    Fix lint

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,23 +4,23 @@ import { type Colors } from '~/interfaces/Colors'
 
 const colors: Record<'light' | 'dark', Colors> = {
   dark: {
+    blueSapphire: '#156778',
+    gray: '#757575',
     lightMist: '#F0F3F6',
     primaryTeal: '#156778',
     slateGray: '#50555C',
     text: '#fff',
-    white: '#fff',
-    blueSapphire: '#156778',
-    gray: '#757575',
+    white: '#fff'
 
   },
   light: {
+    blueSapphire: '#156778',
+    gray: '#757575',
     lightMist: '#F0F3F6',
     primaryTeal: '#156778',
     slateGray: '#50555C',
     text: '#000',
-    white: '#fff',
-    blueSapphire: '#156778',
-    gray: '#757575',
+    white: '#fff'
 
   }
 }
