c43636128bd16b25fb3c1ae2116ceed7fc8e56bc
Truong <truongtdph40209@fpt.edu.vn>
2024-09-26 17:12:45 +0700

    Add GradientBackground component

---
+++ components/molecules/GradientBackground.tsx
@@ -0,0 +1,24 @@
+import React, { type PropsWithChildren } from 'react'
+import { useColorScheme } from 'react-native'
+import { LinearGradient } from 'tamagui/linear-gradient'
+
+import getColors from '~/constants/Colors'
+
+type GradientBackgroundProps = PropsWithChildren<Record<string, unknown>>
+
+const GradientBackground: React.FC<GradientBackgroundProps> = (
+  { children }
+) => {
+  const colors = getColors(useColorScheme())
+
+  return (
+    <LinearGradient
+      colors={[colors.midnightGlow, colors.skyLight]}
+      flex={1}
+    >
+      {children}
+    </LinearGradient>
+  )
+}
+
+export default GradientBackground
