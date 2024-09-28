c9643679bcfc0171979e3c04eec60d91bd0feb06
Truong <truongtdph40209@fpt.edu.vn>
2024-09-28 22:49:59 +0700

    Add new template SignUpTemplate

---
+++ components/templates/SignUpTemplate.tsx
@@ -0,0 +1,64 @@
+import { useRouter } from "expo-router";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, View } from "tamagui";
+
+import ContentTitle from "~/components/atoms/ContentTitle";
+import InputForm from "~/components/molecules/InputForm";
+import LinearGradientBackground from "~/components/molecules/LinearGradientBackground";
+import TextWithLink from "~/components/molecules/TextWithLink";
+import useTranslation from "~/hooks/useTranslation";
+
+const SignUpTemplate: React.FC = (): JSX.Element => {
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  const ButtonSignUp = (): void => {
+    router.replace("/(tabs)/home");
+  };
+
+  return (
+    <LinearGradientBackground>
+      <SafeAreaView style={styles.container}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View marginTop={"13%"}>
+            <ContentTitle
+              title={t("screens.signUp.createAnAccount")}
+              subtitle={t("screens.signUp.signUpPrompt")}
+            />
+          </View>
+
+          <View marginTop={"15%"}>
+            <InputForm
+              visibleInputWithIcons={true}
+              visibleForgotPassword={false}
+              visibleSpace={false}
+              onLoginPress={ButtonSignUp}
+              onLoginGooglePress={ButtonSignUp}
+              positiveButtonTitle={t("screens.login.joinNow")}
+              negativeButtonTitle={t("screens.signUp.joinWithGoogle")}
+            />
+          </View>
+
+          </ScrollView>
+
+          <View  justifyContent="center">
+            <TextWithLink
+              heading={t("screens.signUp.alreadyHaveAnAccount")}
+              linkText={t("screens.login.signIn")}
+            />
+          </View>
+      </SafeAreaView>
+    </LinearGradientBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
+
+export default SignUpTemplate;
