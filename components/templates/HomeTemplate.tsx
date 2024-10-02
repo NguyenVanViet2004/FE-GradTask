11cef97e8b0c97128038c5818c8ae0bb669d92fa
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-10-02 18:59:06 +0700

    Test call api by axios

--- components/templates/HomeTemplate.tsx
+++ components/templates/HomeTemplate.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text, View } from 'tamagui'
 
+import { request } from '~/apis/HttpClient'
 import { useAppFonts } from '~/hooks/useAppFonts'
 
 const HomeTemplate = (): React.ReactElement => {
   const { fonts } = useAppFonts()
+
+  useEffect(() => {
+    const getData = async (): Promise<void> => {
+      try {
+        const res = await request.get('/vouchers')
+        console.log(res)
+      } catch (e: any) {
+        console.error(e)
+      }
+    }
+    void getData()
+  }, [])
 
   return (
     <View
