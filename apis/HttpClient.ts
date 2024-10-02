2fce2e94bd081b481e1a62e50382858542ba895c
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-10-02 17:55:00 +0700

    Configure axios and create method HTTP

--- apis/HttpClient.ts
+++ apis/HttpClient.ts
@@ -1,1 +1,102 @@
-// ??? see what? i will kill you :)
+import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import axios from 'axios'
+
+import store from '~/redux/store'
+
+const BASE_URL = 'https:/growup/api/'
+
+const headers = {
+  'Content-Type': 'application/json',
+  accept: '*/*'
+}
+
+export interface Response<T = any> {
+  success: boolean
+  message: string
+  result: T
+}
+
+export type MyResponse<T = any> = Promise<Response<T>>
+
+class HttpClient {
+  protected readonly axiosInstance: AxiosInstance
+
+  public constructor () {
+    this.axiosInstance = axios.create({
+      baseURL: BASE_URL,
+      headers,
+      timeout: 15000
+    })
+
+    this._initializeResponseInterceptor()
+  }
+
+  private readonly _initializeResponseInterceptor = (): any => {
+    this.axiosInstance.interceptors.request.use(
+      async (config): Promise<any> => {
+        const token = store.getState().auths.token
+        config.headers.Authorization = `${token}`
+        return config
+      },
+      (error) => {
+        Promise.reject(error).catch(e => { console.error(e) })
+      }
+    )
+
+    this.axiosInstance.interceptors.response.use(
+      (config) => {
+        return config?.data
+      },
+      (error) => {
+        let errorMessage = ''
+
+        console.dir(error?.response?.data?.message)
+        console.dir(error)
+
+        if ((error?.message as string)?.includes('Network Error')) {
+          errorMessage = 'Network Error'
+        } else {
+          errorMessage = error?.response?.data?.message ?? error?.message
+          console.dir(errorMessage)
+        }
+
+        return {
+          message: errorMessage,
+          result: null,
+          status: false
+        }
+      }
+    )
+  }
+
+  public get = async <T>(
+    url: string,
+    params = {},
+    config: AxiosRequestConfig = {}
+  ): MyResponse<T> => await this.axiosInstance.get(url, { params, ...config })
+
+  public post = async <T>(
+    url: string,
+    data: any = {},
+    config: AxiosRequestConfig = {}
+  ): MyResponse<T> => await this.axiosInstance.post(url, data, { ...config })
+
+  public put = async <T>(
+    url: string,
+    data: any = {},
+    config: AxiosRequestConfig = {}
+  ): MyResponse<T> => await this.axiosInstance.put(url, data, { ...config })
+
+  public patch = async <T>(
+    url: string,
+    data: any = {},
+    config: AxiosRequestConfig = {}
+  ): MyResponse<T> => await this.axiosInstance.patch(url, data, { ...config })
+
+  public delete = async <T>(
+    url: string,
+    config: AxiosRequestConfig = {}
+  ): MyResponse<T> => await this.axiosInstance.delete(url, { ...config })
+}
+
+export const request = new HttpClient()
